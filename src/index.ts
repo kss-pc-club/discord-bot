a2d7ccee91d884c88214bfa1e97fe910149570fc
Ryoga.exe <ryoga.dev@gmail.com>
2022-04-05 03:09:47 +0900

    update src/index.ts: fix duplicated commands

--- src/index.ts
+++ src/index.ts
@@ -54,8 +54,10 @@ client.once('ready', async () => {
     // })
 
     // スラッシュコマンドを登録
-    if (process.env.SERVER_ID)
-      await client.application?.commands.set([Ping.Data, Register.Data, Update.Data], process.env.SERVER_ID);
+    if (process.env.SERVER_ID) {
+      const guild = await client.guilds.cache.get(process.env.SERVER_ID);
+      await guild?.commands.set([Ping.Data, Register.Data, Update.Data]);
+    }
   }
 });
 
